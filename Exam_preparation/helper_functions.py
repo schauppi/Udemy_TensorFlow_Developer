43fab81aebafbe4d6f2503928f61fcc947c10df2
schauppi <46676413+schauppi@users.noreply.github.com>
2021-09-06 17:20:09 +0200

    added nlp functions

--- Exam_preparation/helper_functions.py
+++ Exam_preparation/helper_functions.py
@@ -4,6 +4,10 @@ import numpy as np
 import tensorflow as tf
 import os
 
+"""
+TIME SERIES FORECASTING
+"""
+
 def plot_time_series(timesteps, values, format=".", start=0, end=None, label=None):
   """
   Plots timesteps against values
@@ -94,3 +98,44 @@ def evaluate_preds(y_true, y_pred):
           "mse": mse.numpy(),
           "rmse": rmse.numpy(),
           "mape": mape.numpy()}
+
+"""
+NATURAL LANGUAGE PROCESSING
+"""
+
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+def calculate_results(y_true, y_pred):
+  """
+  Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+
+  Args:
+  -----
+  y_true = true labels in the form of a 1D array
+  y_pred = predicted labels in the form of a 1D array
+
+  Returns a dictionary of accuracy, precision, recall, f1-score.
+  """
+  # Calculate model accuracy
+  model_accuracy = accuracy_score(y_true, y_pred) * 100
+  # Calculate model precision, recall and f1 score using "weighted" average
+  model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+  model_results = {"accuracy": model_accuracy,
+                  "precision": model_precision,
+                  "recall": model_recall,
+                  "f1": model_f1}
+  return model_results
+
+
+def plot_training_curve(history):
+  plt.figure(figsize=(10,7))
+  loss = history.history["loss"]
+  val_loss = history.history["val_loss"]
+  accuracy = history.history["accuracy"]
+  val_accuracy = history.history["val_accuracy"]
+  plt.plot(loss, label="loss")
+  plt.plot(val_loss, label="val_loss")
+  plt.plot(accuracy, label="accuracy")
+  plt.plot(val_accuracy, label="val_accuracy")
+  plt.legend()
+  plt.title("Training curve")
